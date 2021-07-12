6cd7e9249c9ba5e545821ecb4cdd0a9e89680a46
iiizadora <oliveiras.izadora@gmail.com>
2021-07-12 17:12:35 -0300

    ajustes de grid na tela

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+
+/* Reset*/
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
   body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    
+    font-size: sans-serif;
+    background: #D9E6F6
+  }
+
+  #__next {
+    display:flex;
+    min-height: 100vh;
+    flex-direction:column;
   }
 `
 
@@ -13,6 +28,7 @@ const theme = {
     primary: '#0070f3',
   },
 }
+
 
 export default function App({ Component, pageProps }) {
   return (
